3b51a137b2129203a845aac3cafb3a72785a726e
Joshua Patrick <jjpatric@ualberta.ca>
2020-01-28 17:53:32 -0700

    Added initail files

---
+++ loveClock/loveClock.py
@@ -0,0 +1,77 @@
+#!python3.6
+
+import datetime
+import time
+
+firstmet = datetime.datetime(2018,3,3,22,30)
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+#def getNow():
+#    return datetime.datetime.now()
+
+print("Hello my love! \n\n\nI made this program to show you how much I love every "
+        "single moment I get to spend with you <3\n\n\n")
+
+flag = 0
+
+while(True):
+    if flag == 0:
+        input("How much you you like me?")
+        print("This is how much I like you")
+        flag = 1
+        time.sleep(3.5)
+    
+    print("               ------                     -------               ")
+    print("             ./      \.                 ./       \.             ")
+    print("           ./          \.             ./           \.           ")
+    print("         ./              \.         ./               \.         ")
+    print("       ./                  \.     ./                   \.       ")
+    print("       |                     \___/                       |      ")
+    print("       |                                                 |      ")
+    print("        \                                               /       ")
+    print("         \                                             /        ")
+    print("          \                                           /         ")
+    print("           \                                         /          ")
+    print("            \.                                     ./           ")
+    print("              \.                                 ./             ")
+    print("                \..                           ../               ")
+    print("                   \..                     ../                  ")
+    print("                      \_                 _/                     ")
+    print("                        \_             _/                       ")
+    print("                          \__       __/                         ")
+    print("                             \_   _/                            ")
+    print("                               ---                              ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
